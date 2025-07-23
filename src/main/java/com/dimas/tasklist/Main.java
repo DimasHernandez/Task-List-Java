159d052619fa3f138e3cbe3300ce91be5d89bf7d
DimasHernandez <dimas.hernandez@utp.edu.co>
2025-07-23 12:31:24 -0500

    Validating processing time

--- src/main/java/com/dimas/tasklist/Main.java
+++ src/main/java/com/dimas/tasklist/Main.java
@@ -1,5 +1,7 @@
 package com.dimas.tasklist;
 
+import com.dimas.tasklist.generatedata.GenerateData;
+import com.dimas.tasklist.generateid.UuidGenerateTaskId;
 import com.dimas.tasklist.models.Task;
 import com.dimas.tasklist.services.TaskList;
 import com.dimas.tasklist.services.TaskListImpl;
@@ -13,12 +15,37 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        TaskList taskList = new TaskListImpl();
+
+        int numberOfUsers = 4000;
+        int minTaskPerUser = 3000;
+        int maxTaskPerUser = 5000;
+
+        // Population data
+        GenerateData generateData = new GenerateData(new UuidGenerateTaskId());
+        try {
+
+            // TODO: Borrar esto
+            long timeStartPopulationMilli = System.nanoTime() / 1000000;
+            long timeStartPopulationSeconds = System.nanoTime() / 1000000000;
+            System.out.println("timeStartPopulationMilli = " + timeStartPopulationMilli);
+            System.out.println("timeStartPopulationSeconds = " + timeStartPopulationSeconds);
+
+            generateData.populationData(numberOfUsers, minTaskPerUser, maxTaskPerUser);
+
+            long timeEndPopulationMilli = System.nanoTime() / 1000000;
+            long timeEndPopulationSeconds = System.nanoTime() / 1000000000;
+            System.out.println("Tiempo que demoro en cargar la data en milisegundos " + (timeEndPopulationMilli - timeStartPopulationMilli));
+            System.out.println("Tiempo que demoro en cargar la data en segundos " + (timeEndPopulationSeconds - timeStartPopulationSeconds));
+
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        TaskList taskList = new TaskListImpl(generateData.usersMap, generateData.taskMap);
 
         int option = 0;
 
         do {
-            System.out.println("******************** Bienvenido al Sistema de Tareas ********************");
+            System.out.println("\n ******************** Bienvenido al Sistema de Tareas ********************");
             System.out.println("Por favor seleccione una opción, para continuar.");
             System.out.println("1. Agregar tarea" + '\n' + "2. Actualizar tarea" + '\n' + "3. Eliminar una tarea" + '\n' +
                     "4. Listar tarea" + '\n' + "5. Agregar un usuario" + '\n' + "6. Salir");
@@ -67,8 +94,23 @@ public class Main {
                         System.out.println("Ingresa el id de la tarea");
                         String taskIdRemoved = sc2.next();
 
+                        // TODO Borrar esto
+                        long startTimeMillisCase3 = (System.nanoTime() / 1000000);
+                        long startTimeSecondsCase3 = (System.nanoTime() / 1000000000);
+                        System.out.println("Tiempo de inicio en : " + startTimeMillisCase3 + " milisegundos");
+                        System.out.println("Tiempo de inicio en : " + startTimeSecondsCase3 + " segundos");
+
                         try {
                             taskList.deleteTask(taskIdRemoved);
+
+                            // TODO: Borrar esto
+                            long endTimeMillisCase3 = (System.nanoTime() / 1000000);
+                            long endTimeSecondsCase3 = (System.nanoTime() / 1000000000);
+                            System.out.println("Tiempo de finalización en: " + endTimeMillisCase3 + " milisegundos");
+                            System.out.println("Tiempo de finalización en: " + endTimeSecondsCase3 + " segundos");
+                            System.out.println("EL case 4. Eliminar tarea tardó: " + (endTimeMillisCase3 - startTimeMillisCase3) + " milisegundos");
+                            System.out.println("EL case 4. Eliminar tarea tardó: " + (endTimeSecondsCase3 - startTimeSecondsCase3) + " segundos");
+                            System.out.println("EL case 4. Eliminar tarea tardó: " + ((endTimeSecondsCase3 - startTimeSecondsCase3) / 60) + " minutos");
                         } catch (Exception ex) {
                             System.out.println(ex.getMessage());
                         }
@@ -85,6 +127,12 @@ public class Main {
                                 + '\n' + "[ priority_asc, priority_desc, create_date_asc, create_date_desc]");
                         String orderBy = sc2.next();
 
+                        // TODO Borrar esto
+                        long startTimeMillis = (System.nanoTime() / 1000000);
+                        long startTimeSeconds = (System.nanoTime() / 1000000000);
+                        System.out.println("Tiempo de inicio en : " + startTimeMillis + " milisegundos");
+                        System.out.println("Tiempo de inicio en : " + startTimeSeconds + " segundos");
+
                         try {
                             List<Task> tasks = taskList.getTasksByUserId(taskIdUser, filterStatus, orderBy);
 
@@ -93,6 +141,16 @@ public class Main {
                                 break;
                             }
                             tasks.forEach(System.out::println);
+                            System.out.println("Size list of tasks: " + tasks.size());
+
+                            // TODO: Borrar esto
+                            long endTimeMillis = (System.nanoTime() / 1000000);
+                            long endTimeSeconds = (System.nanoTime() / 1000000000);
+                            System.out.println("Tiempo de finalización en: " + endTimeMillis + " milisegundos");
+                            System.out.println("Tiempo de finalización en: " + endTimeSeconds + " segundos");
+                            System.out.println("EL case 4. Listar tarea tardó: " + (endTimeMillis - startTimeMillis) + " milisegundos");
+                            System.out.println("EL case 4. Listar tarea tardó: " + (endTimeSeconds - startTimeSeconds) + " segundos");
+                            System.out.println("EL case 4. Listar tarea tardó: " + ((endTimeSeconds - startTimeSeconds) / 60) + " minutos");
                         } catch (Exception ex) {
                             System.out.println(ex.getMessage());
                         }
